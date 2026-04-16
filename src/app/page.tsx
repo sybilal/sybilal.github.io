27ec3ec47972903832dfa564a54cdddd9664bbc2
Bilal Syed <sbilalahmed95@gmail.com>
2026-04-16 21:05:04 +0200

    slash fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH?.replace(/\/$/, "") || "";
+
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src={`${basePath}/next.svg`}
           alt="Next.js logo"
           width={100}
           height={20}
