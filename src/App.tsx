62c0f882a3d4536d3475c3c51bc0f579c8e558e3
Bilal Syed <sbilalahmed95@gmail.com>
2026-04-17 19:07:12 +0200

    lock fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <>
+    
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
